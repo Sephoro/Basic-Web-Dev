537009b02c4bd4702f0d65a83ac7a5c3fd19e460
sephoro <eliassepuru@gmail.com>
2019-04-17 22:18:57 +0200

    Created student list table and styled

--- liveServer/class-list-app.js
+++ liveServer/class-list-app.js
@@ -33,35 +33,33 @@ let students = [
   {
     name: 'Kwezi',
     studentNumber: 453528,
-    yearOfStudey: 4,
+    yearOfStudy: 4,
     electives: [electiveOne, electiveTwo, electiveThree]
   },
   {
     name: 'Pieter',
     studentNumber: 454345,
-    yearOfStudey: 4,
+    yearOfStudy: 4,
     electives: [electiveOne, electiveTwo, electiveFour]
 
   },
   {
     name: 'Jade',
     studentNumber: 678345,
-    yearOfStudey: 4,
+    yearOfStudy: 4,
     electives: [electiveTwo, electiveThree, electiveFour]
   },
   {
     name: 'Kiren',
     studentNumber: 567893,
-    yearOfStudey: 4,
+    yearOfStudy: 4,
     electives: [electiveOne, electiveTwo, electiveThree]
   }
 
 ]
 
 // Init the list
-let list = document.createElement('ol')
-let studentsDiv = document.getElementById('students')
-studentsDiv.appendChild(list)
+let table = document.getElementById('st-table')
 
 // Adding students to List
 let DBaddButton = document.getElementById('DBadd')
@@ -69,11 +67,21 @@ let iter = 0
 
 DBaddButton.addEventListener('click', function () {
   if (iter !== (students.length)) {
-    let entry = document.createTextNode(students[iter].name)
-    let li = document.createElement('li')
-    li.appendChild(entry)
-    list.appendChild(li)
-
+    // TODO optimise this process
+    let td1 = document.createElement('td')
+    td1.appendChild(document.createTextNode(students[iter].name))
+    let td2 = document.createElement('td')
+    td2.appendChild(document.createTextNode(students[iter].studentNumber))
+    let td3 = document.createElement('td')
+    td3.appendChild(document.createTextNode(students[iter].yearOfStudy))
+    let td4 = document.createElement('td')
+    td4.appendChild(document.createTextNode(students[iter].electives[0].courseCode + ', ' + students[iter].electives[1].courseCode + ', ' + students[iter].electives[2].courseCode))
+    let tr = document.createElement('tr')
+    tr.appendChild(td1)
+    tr.appendChild(td2)
+    tr.appendChild(td3)
+    tr.appendChild(td4)
+    table.appendChild(tr)
     iter++
   } else {
     alert('Maximum number of students reached from Database reached! Add new students?\n')
@@ -81,11 +89,11 @@ DBaddButton.addEventListener('click', function () {
 }, false)
 
 // Live Filtering
-let lit = list.getElementsByTagName('li')
+let lit = table.getElementsByTagName('tr')
 document.querySelector('#search-text').addEventListener('input', function () {
   let input = document.getElementById('search-text').value.toUpperCase()
 
-  for (let i = 0; i < lit.length; i++) {
+  for (let i = 1; i < lit.length; i++) {
     if (lit[i].textContent.toUpperCase().indexOf(input) > -1) {
       lit[i].style.display = ''
     } else {
@@ -98,8 +106,19 @@ document.querySelector('#search-text').addEventListener('input', function () {
 let addButton = document.getElementById('add-student')
 
 addButton.addEventListener('click', function () {
-  let entry = document.getElementById('new-student-text').value
-  let li = document.createElement('li')
-  li.appendChild(document.createTextNode(entry))
-  list.appendChild(li)
+  // TODO Optimise this process
+  let td1 = document.createElement('td')
+  td1.appendChild(document.createTextNode(document.getElementById('new-student-text').value))
+  let td2 = document.createElement('td')
+  td2.appendChild(document.createTextNode(document.getElementById('new-st-number').value))
+  let td3 = document.createElement('td')
+  td3.appendChild(document.createTextNode(document.getElementById('new-yos').value))
+  let td4 = document.createElement('td')
+  td4.appendChild(document.createTextNode(document.getElementById('new-electives').value))
+  let tr = document.createElement('tr')
+  tr.appendChild(td1)
+  tr.appendChild(td2)
+  tr.appendChild(td3)
+  tr.appendChild(td4)
+  table.appendChild(tr)
 }, false)
